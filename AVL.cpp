64e9784e6fc722f2ea8703381d3ed2162e418d97
Asadullah Azimi <asadullahazimi@Asadullahs-MacBook-Pro-46.local>
2024-10-19 17:28:55 -0400

    Implementing the right rotaion, insertion, inorder traversal, and a simple test case

--- AVL.cpp
+++ AVL.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Node
 {
     public:
-        int key;
+        int value;
         Node* parent;
         Node* left_child;
         Node* right_child;
@@ -21,7 +21,7 @@ class AVL
 
         // Helper methods
         int height(Node* node);
-        int balance_factor(Node* node);
+        int get_balance_factor(Node* node);
         Node* left_rotate(Node* node);
         Node* right_rotate(Node* node);
         Node* left_right_rotate(Node* node);
@@ -34,8 +34,8 @@ class AVL
         void postorder(Node* root);
 
         // Insertion and Deletion
-        Node* insert(Node* root, int value);
-        Node* delete_node(Node* root, int value);
+        Node* insert(Node* node, int value);
+        Node* delete_node(Node* node, int value);
 
         // Traversal
         void inorder(Node* root);
@@ -57,13 +57,7 @@ class AVL
 */
 int AVL::height(Node* node)
 {
-    // If the node exist and the left child exist, return the height of the left child
-    int h_left = (node && node -> left_child) ? node -> left_child -> height : 0;
-    // If the node exist and the right child exist, return the height of the right child
-    int h_right = (node && node -> right_child) ? node -> right_child -> height : 0;
-
-    // Return the maximum height of the children + 1
-    return (h_left > h_right) ? h_left + 1 : h_right + 1;
+    return node ? node -> height : 0;
 }
 
 
@@ -73,17 +67,15 @@ int AVL::height(Node* node)
 
 /* Helper methods
  * Returns the balance factor of the node
- * The balance factor of a node is the height of the left child - the height of the right child
+ * The balance factor of a node is the height of the left child - the height
+ * of the right child
 */
-int AVL::balance_factor(Node* node)
+int AVL::get_balance_factor(Node* node)
 {
-    // If the node exist and the left child exist, return the height of the left child
-    int h_left = (node && node -> left_child) ? node -> left_child -> height : 0;
-    // If the node exist and the right child exist, return the height of the right child
-    int h_right = (node && node -> right_child) ? node -> right_child -> height : 0;
-
+    int Rh = (node && node -> right_child) ? node -> right_child -> height : 0;
+    int Lh = (node && node -> left_child) ? node -> left_child -> height : 0;
     // Return the balance factor of the node
-    return h_left - h_right;
+    return  Lh - Rh;
 }
 
 
@@ -99,23 +91,166 @@ int AVL::balance_factor(Node* node)
 Node* AVL::left_rotate(Node* node)
 {
     // Create a new node to store the left child of the node
-    Node* node_left = node -> left_child;
+    Node* right_node = node -> right_child;
     // Create a new node to stor the left child of the left child of the node
-    Node* node_left_child_right_child = node_left -> right_child;
+    Node* right_node_left_child = right_node -> left_child;
 
     // Set the left child of the new node to be the node
-    node_left -> right_child = node;
+    right_node -> left_child = node;
     // Set the right child of the node to be the left child of the left child of the node
-    node -> left_child = node_left_child_right_child;
+    node -> right_child = right_node_left_child;
 
     // Update the height of the node
-    node -> height = height(node);
+    node -> height = max(height(node -> left_child), height(node -> right_child)) + 1;
     // Update the height of the new node
-    node_left -> height = height(node_left);
+    right_node -> height = max(height(right_node -> left_child), height(right_node -> right_child)) + 1;
 
     // Update the root of the tree
-    if (node == root) root = node_left;
+    if (node == root) root = right_node;
 
     // Return the new node
-    return node_left;
+    return right_node;
+}
+
+
+
+
+
+/*
+ * Helper method
+ * Performs a right rotation on the node
+ * Returns the new node
+*/
+Node* AVL::right_rotate(Node* node)
+{
+    // Create a new pointer to point to the left child of the node
+    Node* left_node = node -> left_child;
+    // Create a new pointer to point to the right child of the left child of the node
+    Node* left_node_right_child = left_node -> right_child;
+    // Set the right child of the left_node to be the node
+    left_node -> right_child = node;
+    // Set the left child of the node to be the right child of the left child of the node
+    node -> left_child = left_node_right_child;
+
+    // Update the height of the node
+    node -> height = max(height(node -> left_child), height(node -> right_child)) + 1;
+    // Update the height of the new root
+    left_node -> height = max(height(left_node -> left_child), height(left_node -> right_child)) + 1;
+
+    // Update the root of the tree
+    if (node == root) root = left_node;
+
+    // Return the new root
+    return left_node;
+}
+
+
+
+
+
+
+
+/*
+ * Helper method
+ * Inserts a new node into the tree
+ * Returns the new node
+*/
+
+Node* AVL::insert(Node* node, int value)
+{
+    // If the root is NULL, create a new node and set its key to the value
+    if (!node)
+    {
+        Node* new_node = new Node();
+        new_node -> value = value;
+        new_node -> height = 1;
+        return new_node;
+    }
+
+    // If the value is less than the key of the root, insert the value into the
+    // left child of the root and update the height of the root
+    if (value < node -> value)
+    {
+        node -> left_child = insert(node -> left_child, value);
+    }
+    else if (value > node -> value)
+    {
+        // If the value is greater than the value of the root, insert the value into the
+        // right child of the node
+        node -> right_child = insert(node -> right_child, value);
+    }
+    else
+    {
+        // If the value is equal to the value of the root, return the root
+        // Equal values are not allowed in BST
+        return node;
+    }
+
+
+    // Update the height of the root
+    node -> height = max(height(node -> left_child), height(node -> right_child)) + 1;
+
+
+    // Get the balance factor
+    int balance_factor = get_balance_factor(node);
+
+    // Balancing the tree
+    // Left Left Case
+    if (balance_factor > 1 && value < node -> left_child -> value) return right_rotate(node);
+
+    // Right Right Case
+    if (balance_factor < -1 && value > node -> right_child -> value) return left_rotate(node);
+
+    // Left Right Case
+    if (balance_factor > 1 && value > node -> left_child -> value)
+    {
+        node -> left_child = left_rotate(node -> left_child);
+        return right_rotate(node);
+    }
+
+    // Right Left Case
+    if (balance_factor < -1 && value < node -> right_child -> value)
+    {
+        node -> right_child = right_rotate(node -> right_child);
+        return left_rotate(node);
+    }
+
+    // Return the root
+    return node;
+}
+
+
+
+
+/*
+ * Helper method
+ * Traverses the tree in inorder fashion and prints the values of the nodes
+ * Returns the node with the minimum value
+*/
+void AVL::inorder(Node* root)
+{
+    if (root)
+    {
+        inorder(root -> left_child);
+        cout << root -> value << " ";
+        inorder(root -> right_child);
+    }
+
+}
+
+
+
+
+int main()
+{
+    AVL avl;
+    avl.root = avl.insert(avl.root, 10);
+    avl.root = avl.insert(avl.root, 20);
+    avl.root = avl.insert(avl.root, 30);
+    avl.root = avl.insert(avl.root, 40);
+    avl.root = avl.insert(avl.root, 50);
+    avl.root = avl.insert(avl.root, 25);
+
+    avl.inorder();
+    return 0;
 }
